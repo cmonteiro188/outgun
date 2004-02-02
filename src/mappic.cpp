72765f1e579ec2c940d7dfc196b644e1eb7564ed
Jani Rivinoja <janir@mbnet.fi>
2004-02-02 21:14:00 +0200

    Save minimap pictures to the mappics subdirectory

--- src/mappic.cpp
+++ src/mappic.cpp
@@ -48,7 +48,7 @@ void Mappic::save_pictures() const {
 	set_color_depth(16);
 	Graphics graphics;
 	graphics.setcolors();
-	string dir("screens");
+	string dir("mappics");
 	dir += directory_separator;
 	for (vector<string>::const_iterator name = smaps.begin(); name != smaps.end(); name++) {
 		string picture = dir + *name + ".pcx";
