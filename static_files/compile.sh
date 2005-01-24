7ca4c9ef1b8deaee81f702277f1f28a80811f8cd
Thomaz de Oliveira dos Reis <thommy@globo.com>
2005-01-24 22:31:00 +0200

    Update the compile script

--- static_files/compile.sh
+++ static_files/compile.sh
@@ -2,6 +2,7 @@
 #
 # Compile script for Outgun 1.0.0, and may work in earlier versions.
 # by ThOR27 - Thomaz de Oliveira dos Reis - thommy@globo.com
+#
 
 # This function was taken from the wineinstall script.
 # for more information about what is wine www.winehq.org
@@ -15,26 +16,163 @@ function conf_yesno_answer {
   done
 }
 
-echo "This script will compile outgun in your machine"
-echo "This script is still BETA version. And it's released under the GPL"
-echo "Please, Report any errors to the FORUM at outgun.sf.net or by email at: thor27@gmail.com"
+function downandinstall {
+	name=$1 
+	compile=$2
+	install=$3 
+	siteurl=$4
+	fileurl=$5
+	filename=$6
+	subdir=$7
+
+	echo "Couldn't compile with $name, probabbly you don't have $name or $name-dev installed on your system"
+	echo "you can try to download any package for your distribution in your distro repository" 
+	echo "You can check at $siteurl"
+	echo "But if you prefer, this script will try to download $name source, compile and install."
+	echo "For the installation process you may need your root password"
+	echo
+	echo "Do you wan't the script to try downloading and installing $name?"
+
+	conf_yesno_answer "(yes/no) "
+ 
+	 if [ "$ANSWER" = 'yes' ]; 
+	 then {
+		
+		if ! [ -f "$filename" ];
+		then {
+			wget "$fileurl"
+		} fi
+		
+		if [ -f "$filename" ];
+		 then {
+			if [ "`tar -zxf "$filename" 2>&1`" == "" ];
+			then {
+				cd $subdir
+				if { $compile; }
+				then {
+					if [ `whoami` = 'root' ]
+					then {
+						$install
+					}
+					else {
+						if ! su root -c "$install"
+						then {
+							if ! su root -c "$install"
+							then {
+								echo
+								echo "Incorrect password or problem installing hawkNL"
+								exit 1
+	          					} fi
+						} fi
+					} fi
+					echo
+					echo "$name compiled. Running the script again to see if everything is OK."
+					echo
+					cd ../
+					sh ./compile.sh
+				}
+				else {
+					echo "couldn't compile $name, see the messages and try to fix yourself"
+				} fi 
+			} 
+			else {
+				echo "Problems uncompressing $name, try downloading it again"
+				rm -fr "$filename"
+			} fi
+		} 
+		else {
+			echo "Problems downloading the $name, check you internet connection and try again"
+		} fi
+	 
+	}
+	else {
+		echo "OK, so install $name or $name-dev in your system and run this script again"
+	} fi
+}
+
+function clean {
+	rm -fr ./allegro.check.cpp
+	rm -fr ./hawk.check.cpp
+	rm -fr ./g++.check.cpp
+	rm -fr ./allegro.check.bin
+	rm -fr ./hawk.check.bin
+	rm -fr ./g++.check.bin
+}
+
 echo
-echo Checking g++
-echo Compiling. . .
+echo ===========================================================================================
+echo "This script will try to compile outgun in your machine and their dependecies if needed"
+echo "It's released under the GPL. Read docs for more info"
+echo "WARNING: This script is still BETA version."
+echo "Please, Report any errors to the FORUM at outgun.sf.net or by email at: thommy@globo.com"
+echo ===========================================================================================
+echo
+echo "Cleaning files from a old configuration. . ."
+clean;
+echo
+echo "Creating CPP files that will be needed for checking. . ."
+
+#=====================================
+# HawkNL Checker by Nix - Niko Ritari
+#=====================================
+cat > ./hawk.check.cpp <<EOF
+#include <nl.h>
+ 
+int main() {
+    return nlInit() == NL_FALSE;
+}
+
+EOF
+#=====================================
+
+#=====================================
+# Allegro Checker by Nix - Niko Ritari
+#=====================================
+cat > ./allegro.check.cpp <<EOF
+#include <allegro.h>
+
+int main() {
+    (void)GFX_XWINDOWS_FULLSCREEN;
+    allegro_init();
+    return 0;
+}
+
+EOF
+#=====================================
+
+#=====================================
+# g++ Checker by Nix - Niko Ritari
+#=====================================
+cat > ./g++.check.cpp <<EOF
+#include <sstream>
+
+using std::istringstream;
+int main() {
+        for (int i = 0; false; );
+        for (int i = 0; false; );
+        return 0;
+}
+
+EOF
+#=====================================
+
+echo
+echo "Checking g++"
+echo "Compiling. . ."
 g++ g++.check.cpp -o g++.check.bin
 if [ -f "./g++.check.bin" ];
 then {
-	echo g++ compiled!
+	echo "g++ compiled!"
 	echo
-	echo Checking HawkNL
-	echo Compiling. . .
+	echo "Checking HawkNL"
+	echo "Compiling. . ."
 	
-	g++ hawknl.check.cpp -o hawk.check.bin -lNL -pthread
+	g++ hawk.check.cpp -o hawk.check.bin -lNL -pthread
 
 	if [ -f "./hawk.check.bin" ];
 	then {
-		echo Compiled!
-		echo Running. . .
+		echo "Compiled!"
+		echo "Running. . ."
 	
 		if { ./hawk.check.bin; }
 		then {
@@ -43,18 +181,20 @@ then {
 		}	
 		else {
   			echo "WARNING: Problems running a program compiled with HawkNL. Outgun should compile but will not run."
-			echo "	       Try to get a newer version of HawkNL (http://www.hawksoft.com/hawknl/) and test it again,"
+			echo "         Try to get a newer version of HawkNL (http://www.hawksoft.com/hawknl/) and test it again,"
 			echo "         or symlink files for the filename that Outgun will be linked to."
+			echo
 		} fi
-		echo Checking Allegro	
-		echo Compiling. . .
+		echo
+		echo "Checking Allegro"	
+		echo "Compiling. . ."
 			
 		g++ allegro.check.cpp -o allegro.check.bin `allegro-config --libs`
 			
 		if [ -f "./allegro.check.bin" ];
 		then {
-			echo Compiled!
-			echo Running. . .
+			echo "Compiled!"
+			echo "Running. . ."
 				
 			if [ "`./allegro.check.bin 2>&1`" == "" ]; 
 			then {
@@ -63,21 +203,23 @@ then {
 			}
 			else {
 		  		echo "WARNING: Problems running a program compiled with Allegro. Outgun should compile but will not run."
-				echo "	       Try to get a newer version of Allegro (http://alleg.sf.net/) and test it again,"
+				echo "         Try to get a newer version of Allegro (http://alleg.sf.net/) and test it again,"
 				echo "         or symlink files for the filename that Outgun will be linked to."
+				echo
 			} fi	
 			
 			echo "Now outgun is going to compiled, if there were no errors or warnings, you should be playing soon."
 			echo "Not so soon, deependig of your machine speed, in my XP2200+ with 512 DDR 400, it takes 10 minutes to this process be completed"
 			echo
-			echo Compiling Outgun. . .
+			echo "Compiling Outgun. . ."
 			cd ./src
 			make LINUX=1
 				
 			if [ -f "../outgun" ];
 			then {
 				echo "Outgun compiled fine. You should run now by typing ./outgun"
-				echo "Nice play"
+				echo
+				echo "Nice play!"
 			}
 			else {
 				echo "I don't know how but outgun didn't compiled"
@@ -86,88 +228,11 @@ then {
 					
 		}
 		else {
-			echo "Couldn't compile with Allegro, probabbly you don't have allegro or allegro-dev installed on your system"
-			echo "you can try to download any package for your distribution in http://alleg.sf.net or on your distro repository"
-			echo "But if you prefer, this script will try to download allegro source, compile and install."
-			echo "For the installation process you may need your root password"
-			echo
-			echo "Do you wan't the script to try downloading and installing allegro?"
-			conf_yesno_answer "(yes/no) "
-			 if [ "$ANSWER" = 'yes' ]; 
-			 then {
-				 wget "http://koti.mbnet.fi/outgun/dependencies/outgun_1.0_allegro_src.tar.gz"
-				 
-			 }
-			 else {
-			 	echo "OK, so install allegro or allegro-dev in your system and run this script again"
-			 } fi
-			
+			downandinstall "Allegro" "./fix.sh unix&&./configure&&make" "make install" "http://alleg.sf.net/" "http://koti.mbnet.fi/outgun/dependencies/outgun_1.0_allegro_src.tar.gz" "./outgun_1.0_allegro_src.tar.gz" "./allegro-4.1.17/"
 		} fi
 	}
 	else {
-			echo "Couldn't compile with HawkNL, probabbly you don't have hawknl or hawknl-dev installed on your system"
-			echo "you can try to download any package for your distribution in your distro repository or the source code" 
-			echo "at http://www.hawksoft.com/hawknl/"
-			echo "But if you prefer, this script will try to download HawkNL source, compile and install."
-			echo "For the installation process you may need your root password"
-			echo
-			echo "Do you wan't the script to try downloading and installing HawkNL?"
-			
-			conf_yesno_answer "(yes/no) "
-			 
-			 if [ "$ANSWER" = 'yes' ]; 
-			 then {
-				
-				if ! [ -f "outgun_1.0_hawknl_src.tar.gz" ];
-				then {
-					wget "http://koti.mbnet.fi/outgun/dependencies/outgun_1.0_hawknl_src.tar.gz"
-			 	}
-				
-				if [ -f "outgun_1.0_hawknl_src.tar.gz" ];
-				 then {
-					if [ "`tar -zxf outgun_1.0_hawknl_src.tar.gz 2>&1`" == "" ];
-					then {
-						cd ./hawknl1.68/
-						if { make -f makefile.linux; }
-						then {
-							if [ `whoami` = 'root' ]
-  							then {
-								make -f makefile.linux install
-							}
-							else {
-							      if ! su root -c "make -f makefile.linux install"
-							      then {
-      									if ! su root -c "make -f makefile.linux install"
-							        	then {
-										echo "Incorrect password"
-										exit 1
-             						         	}fi
-								} fi
-   							} fi
-
-							echo
-							echo "HawkNL compiled. Running the script again to see if everything is OK."
-							echo
-							cd ../
-							sh ./compile.sh
-						}
-						else {
-							echo "couldn't compile HawkNL, see the messages and try to fix yourself"
-						} fi 
-					} 
-					else {
-						echo "Problems uncompressing HawkNL, try downloading it again"
-						rm -fr outgun_1.0_hawknl_src.tar.gz
-					} fi
-				} 
-				else {
-					echo "Problems downloading the HawkNL, check you internet connection and try again"
-				} fi
-			 
-			 }
-			 else {
-			 	echo "OK, so install hawknl or hawknl-dev in your system and run this script again"
-			 } fi
+		downandinstall "hawkNL" "make -f makefile.linux" "make -f makefile.linux install" "http://www.hawksoft.com/hawknl/" "http://koti.mbnet.fi/outgun/dependencies/outgun_1.0_hawknl_src.tar.gz" "./outgun_1.0_hawknl_src.tar.gz" "./hawknl1.68/"
 	} fi
 }
 else {
