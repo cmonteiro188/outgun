9bf73de3ed1aab6c752e1560a596411e4267a7cf
Jani Rivinoja <janir@mbnet.fi>
2006-02-06 16:25:00 +0200

    Update copyright years

--- src/commont.cpp
+++ src/commont.cpp
@@ -3,7 +3,7 @@
  *
  *  Copyright (C) 2002 - Fabio Reis Cecin
  *  Copyright (C) 2003, 2004 - Niko Ritari
- *  Copyright (C) 2003, 2004, 2006 - Jani Rivinoja
+ *  Copyright (C) 2003, 2004 - Jani Rivinoja
  *
  *  This file is part of Outgun.
  *
