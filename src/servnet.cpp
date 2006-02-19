ef980dc20edd9b0394c57938ebf662e641f9222b
Jani Rivinoja <janir@mbnet.fi>
2006-02-20 00:43:01 +0200

    Change map change message to admin shell
    Change the map change message to the admin shell to be in the same
    format as the other server messages, i.e. it has a vertical bar ‘|’ at
    the start.

--- src/servnet.cpp
+++ src/servnet.cpp
@@ -893,7 +893,7 @@ void ServerNetworking::broadcast_map_change_message(int reason, const char* mapn
         writeLong(lebuf, count, STA_GAME_OVER);
         nlWrite(shellssock, lebuf, count);
     }
-    sendTextToAdminShell("Map: " + host->current_map().title);
+    sendTextToAdminShell("| Map is " + host->current_map().title);
 }
 
 void ServerNetworking::broadcast_map_change_info(int votes, int needed, int vote_block_time) const {
