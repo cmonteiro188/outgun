475e6c74f6a6384714d8915e05bd3a2ed7e6aa23
Niko Ritari <npr1@suomi24.fi>
2007-03-19 19:32:05 +0200

    Fix on-screen check

--- src/client.cpp
+++ src/client.cpp
@@ -5193,7 +5193,7 @@ bool Client::on_screen_exact(int rx, int ry, double lx, double ly, double fudge)
 bool Client::player_on_screen(int pid) const {
     if (!fx.player[pid].used)
         return false;
-    else if (!replaying && !fx.player[pid].onscreen && !fx.player[pid].dead && fx.frame > fx.player[pid].posUpdated + 20)
+    else if (!replaying && !fx.player[pid].onscreen && (!fx.player[pid].dead || fx.player[pid].posUpdated < 0) && fx.frame > fx.player[pid].posUpdated + 20)
         return false;
     else
         return on_screen(fx.player[pid].roomx, fx.player[pid].roomy, fx.player[pid].lx, fx.player[pid].ly, Graphics::extended_player_max_size_in_world / 2);
