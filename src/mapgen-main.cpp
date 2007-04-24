e2a4f563debb1f31a0b772cdc35d30f31d5115a5
Jani Rivinoja <janir@mbnet.fi>
2007-04-24 19:28:02 +0300

    Create console program for random map generator

---
+++ src/mapgen-main.cpp
@@ -0,0 +1,15 @@
+#include "mapgen.h"
+
+using std::cout;
+
+int main(int argc, const char* argv[]) {
+    if (argc != 3)
+        cout << "Usage: mapgen width height\n";
+    else {
+        srand(time(0));
+        cout << "Width " << atoi(argv[1]) << ", height " << atoi(argv[2]) << '\n';
+        MapGenerator generator;
+        generator.generate(atoi(argv[1]), atoi(argv[2]), false);
+        generator.draw(cout);
+    }
+}
