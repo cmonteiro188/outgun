42138733d89130d9d5f0d219ee9eb859613fe25b
Niko Ritari <npr1@suomi24.fi>
2008-12-16 00:18:11 +0200

    Improve map_get
    Allow map_get with a valid but not exact key type. Add template classes
    MapReader and AssertingMapReader to provide operator[] for convenience.

--- src/map_with_helpers.h
+++ src/map_with_helpers.h
@@ -28,19 +28,37 @@
 
 class NotInMap { }; // exception
 
-template<class T1, class T2> const T2& map_get(const std::map<T1, T2>& m, const T1& key) throw (NotInMap) {
+template<class T1, class T2, class T1EquivT> const T2& map_get(const std::map<T1, T2>& m, const T1EquivT& key) throw (NotInMap) {
     const typename std::map<T1, T2>::const_iterator i = m.find(key);
     if (i == m.end())
         throw NotInMap();
     return i->second;
 }
 
-template<class T1, class T2> const T2& map_get_assert(const std::map<T1, T2>& m, const T1& key) throw () {
+template<class T1, class T2, class T1EquivT> const T2& map_get_assert(const std::map<T1, T2>& m, const T1EquivT& key) throw () {
     try {
         return map_get(m, key);
     } catch (NotInMap) {
         nAssert(0);
     }
 }
+
+template<class T1, class T2> class MapReader { /// to be used in place of a const map& to provide an operator[]
+    typedef std::map<T1, T2> MapT;
+    const MapT& m;
+
+public:
+    MapReader(const MapT& map) throw () : m(map) { }
+    const T2& operator[](const T1& key) const throw (NotInMap) { return map_get(m, key); }
+};
+
+template<class T1, class T2> class AssertingMapReader { /// to be used in place of a const map& to provide an operator[]
+    typedef std::map<T1, T2> MapT;
+    const MapT& m;
+
+public:
+    AssertingMapReader(const MapT& map) throw () : m(map) { }
+    const T2& operator[](const T1& key) const throw () { return map_get_assert(m, key); }
+};
 
 #endif
